78495476ac5b2b1eb95337edee7ca5f0030dd387
Miguel Alcocer Arjona <malcocer@coma1nt.coma.com.mx>
2024-03-24 00:12:36 -0600

    Se logra generar el pdf

--- Assets/FuncionesReportes.js
+++ Assets/FuncionesReportes.js
@@ -1,4 +1,42 @@
 const formCatalogoProductos = document.getElementById("formCatalogoProductos");
+if (formCatalogoProductos) {
+    formCatalogoProductos.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const departamento = document.getElementById("departamento").value;
+        const precio = document.getElementById("precio").value;
+        const activo = document.getElementById("activo").value;
+
+        const valores = {
+            departamento,
+            precio,
+            activo
+        };
+
+        const URL = "http://localhost/FPDF/CatalogoProductos.php";
+
+        axios.post(URL, valores, {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                responseType: 'blob'
+            })
+            .then((response) => {
+                const blob = new Blob([response.data], { type: 'application/pdf' });
+                const url = window.URL.createObjectURL(blob);
+                window.open(url, '_blank');
+            })
+            .catch((error) => {
+                console.error("Error: " + error.message);
+                alert("Error: " + error.message);
+            });
+    });
+}
+
+
+
+/*CODIGO PARA VALIDAR SI ESTA REGRESANDO LA INFORMACION 
+const formCatalogoProductos = document.getElementById("formCatalogoProductos");
 if(formCatalogoProductos){
     formCatalogoProductos.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -36,3 +74,4 @@ if(formCatalogoProductos){
             });
     })
 }
+*/
--- FPDF/CatalogoProductos.php
+++ FPDF/CatalogoProductos.php
@@ -1,5 +1,90 @@
 <?php
+require_once "./config/dbcontext.php";
+require('./fpdf.php');
 
+$json = file_get_contents("php://input");
+$datos = json_decode($json, true);
+
+if(isset($datos["departamento"], $datos["precio"], $datos["activo"])) {
+    
+    $departamento = $datos["departamento"];
+    $precio = $datos["precio"];
+    $activo = $datos["activo"];
+
+    switch ($precio) {
+        case 'menos_de_500':
+            $precio_min = 0;
+            $precio_max = 500;
+            break;
+        case 'mas_de_500':
+            $precio_min = 500;
+            $precio_max = 999;
+            break;
+        case 'mas_de_1000':
+            $precio_min = 1000;
+            $precio_max = 99999999; 
+            break;
+        default:
+            $precio_min = 0;
+            $precio_max = 99999999;
+            break;
+    }
+
+    $sql = "SELECT Nombre, Descripcion, precio_venta, Stock, fotoproducto FROM productos WHERE IDDepartamento = ? AND precio_venta BETWEEN ? AND ? AND lActivo = ?";
+    
+    $stmt = mysqli_prepare($link, $sql);
+
+    if ($stmt) {
+
+        mysqli_stmt_bind_param($stmt, "iiis", $departamento, $precio_min, $precio_max, $activo);
+        
+    
+        mysqli_stmt_execute($stmt);
+        
+     
+        $resultado = mysqli_stmt_get_result($stmt);
+
+        $pdf = new FPDF();
+        $pdf->AliasNbPages();
+        $pdf->AddPage();
+        $pdf->SetFont('Arial', 'B', 16);
+        $pdf->Ln(10);
+
+        while ($row = mysqli_fetch_array($resultado)) {
+            $nombre = $row['Nombre'];
+            $descripcion = $row['Descripcion'];
+            $stock = $row['Stock'];
+            $precio = $row['precio_venta'];
+            
+
+            $pdf->Ln(60); 
+            $pdf->Cell(0, 10, "Nombre: $nombre", 0, 1, 'C');
+            $pdf->Cell(0, 10, "Descripcion: $descripcion", 0, 1, 'C');
+            $pdf->Cell(0, 10, "Stock: $stock", 0, 1, 'C');
+            $pdf->Cell(0, 10, "Precio: $precio", 0, 1, 'C');
+    
+
+       
+            $pdf->Ln();
+        }
+
+      
+        $pdf->Output("D", "catalogo_productos.pdf");
+
+    } else {
+        die("Error en la preparación de la consulta: " . mysqli_error($link));
+    }
+
+
+    mysqli_stmt_close($stmt);
+    
+    exit;
+}
+?>
+
+
+<?php
+/*CODIGO PARA VALIDAR QUE ESTAN OBTENIENDO LOS DAOTS, DEBE ESTAR MODIFICADO LA SOLICITUD AJAX 
 require_once "./config/dbcontext.php";
 
 $json = file_get_contents("php://input");
@@ -57,4 +142,17 @@ if(isset($datos["departamento"], $datos["precio"],$datos["activo"])) {
 
 //echo json_encode(["Resultado" => $Resultado]); 
 echo json_encode(["Resultado" => $Resultado, "Productos" => $productos]); //Envio de productos con el ok
+*/
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
