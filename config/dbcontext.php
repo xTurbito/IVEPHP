abf01a0a81f2a597269096c9c1a6a90a443e58b2
Hub <miguelalcocer2102@gmail.com>
2024-04-10 11:04:52 -0700

    Se agregan variables de conexion

--- config/dbcontext.php
+++ config/dbcontext.php
@@ -1,8 +1,8 @@
 <?php
-$server= "localhost";
-$user= "root";
-$pass = "";
-$db = "ive";
+$server= "id22029402_ive";
+$user= "id22029402_malcocer";
+$pass = "Frida0208$";
+$db = "id22029402_malcocer";
 $link = mysqli_connect($server, $user, $pass, $db);
 
 
